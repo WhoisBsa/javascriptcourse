18112b4bee020a0ca04b5939613565987adbbc86
WhoisBsa <mbscbsjbs@gmail.com>
2019-06-20 19:41:43 -0300

    aula11

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+console.log('Hello, world!')
+
+var vel = 60.5
+console.log(`The velocity of your car is ${vel}`)
+
+if (vel > 60) {
+    console.log(`Fined`)
+}
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+var country = 'Brazil'
+console.log(`Living at ${country}`);
+
+if(country == 'Brazil') {
+    console.log('You are Braziliam!')
+} else {
+    console.log('You are Foreign');
+}
