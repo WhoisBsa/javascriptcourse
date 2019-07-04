dac6c112b67f097ba76d73f479f54ef16004d5f8
WhoisBsa <mbscbsjbs@gmail.com>
2019-07-04 13:53:25 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+/*
+while (true) {
+    console.log('All Right')
+}
+*/
+
+/*
+var c =1
+while (c <= 5) {
+    console.log('All Right')
+    c++
+}
+*/
+
+do {
+    console.log('All Right')
+    c++
+} while(c <= 5)
