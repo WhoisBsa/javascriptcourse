868869bc9437115206c8f1eaff98944862383730
WhoisBsa <mbscbs@gmail.com>
2020-02-19 13:39:23 -0300

    Aula 14 Ex 17

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -4,9 +4,14 @@ function generateTable() {
     var table  = document.querySelector('textarea#table')
     table.innerHTML = ''
 
-    //Start Loop
-    for(var i = 0; i <= 10; i++){
-        table.innerHTML += `${number.value} * ${i} = ${Number(number.value) * i}\n`
+    if(number.value.length == 0)
+        alert("Type a number")
+    else {
+        var n = Number(number.value)
+
+        //Start Loop
+        for(var i = 0; i <= 10; i++)
+            table.innerHTML += `${n} * ${i} = ${n * i}\n`
     }
 
 }
